3ac58680aa4da650982c4be66d787675158c7434
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-01-14 19:10:40 -0300

    Criando states do form de cadastro

--- frontend/src/pages/Auth/Register.tsx
+++ frontend/src/pages/Auth/Register.tsx
@@ -4,11 +4,41 @@ import "./Auth.css";
 import { Link } from "react-router-dom";
 
 // Hooks
-import { FormEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 const Register = () => {
-  const handleSubmit = (e: FormEvent) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log({ user });
+  };
+
+  const handleOnChangeName = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleOnChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleOnChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleOnChangeConfirmPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
   };
 
   return (
@@ -19,10 +49,30 @@ const Register = () => {
         <label>
           <span>Nome</span>
         </label>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <input type="password" placeholder="Confirme a senha" />
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={handleOnChangeName}
+          value={name || ""}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={handleOnChangeEmail}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={handleOnChangePassword}
+          value={password || ""}
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          onChange={handleOnChangeConfirmPassword}
+          value={confirmPassword || ""}
+        />
         <input type="submit" value="Cadastrar" />
       </form>
       <p>
