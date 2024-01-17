13e7a18ab53fcb03edc9ea6437ca6dca6d8029f8
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-01-16 23:38:38 -0300

    Verificando se usuário está autenticado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,22 +5,42 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Register from "./pages/Auth/Register";
-import Login from "./pages/Auth/Login";
+
+// Components
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+// Pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import { useAuth } from "./hooks/useAuth";
+
 function App() {
+  const { auth, loading } = useAuth();
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div>
       <Router>
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
 
