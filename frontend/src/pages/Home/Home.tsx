6f00724f6c9092c14e2744743df3b7d34ccdd2e2
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-01-26 19:05:51 -0300

    Finalizando home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,10 +3,10 @@ import "./Home.css";
 // Components
 import LikeContainer from "../../components/LikeContainer";
 import PhotoItem from "../../components/PhotoItem";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 // Hooks
-import { useEffect } from "react";
+import { useEffect, MouseEvent } from "react";
 import { useAppSelector } from "../../hooks/useAppSelector";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
@@ -14,8 +14,10 @@ import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 // Redux
 import { getAllPhotos, likePhoto } from "../../slices/photoSlice";
 import { IPhoto } from "../../services/photo/models/IPhoto";
+import { uploadsURL } from "../../utils/config";
 
 const Home = () => {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
   const resetMessage = useResetComponentMessage(dispatch);
@@ -37,12 +39,35 @@ const Home = () => {
     resetMessage();
   };
 
+  const handleSeeMore = (photo: IPhoto) => {
+    navigate(`/photos/${photo._id}`);
+  };
+
   if (statusPhotos === "loading") {
     return <p>Carrengado...</p>;
   }
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="home">
+      {photos.map((photo) => (
+        <div key={photo._id}>
+          <PhotoItem photo={photo} />
+          {user && (
+            <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+          )}
+          <Link className="btn" to={`/photos/${photo._id}`}>
+            Ver mais
+          </Link>
+        </div>
+      ))}
+
+      {photos.length === 0 && (
+        <h2 className="no-photos">
+          Ainda não há fotos publicadas.{" "}
+          <Link to={`/users/${user?._id}`}>
+            Clique aqui para postar a primeira foto!
+          </Link>
+        </h2>
+      )}
     </div>
   );
 };
